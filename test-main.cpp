a6a59bec448534a78e15e3e7ec2689dc8905c177
Andrey <marfajuc@gmail.com>
2023-08-25 22:18:07 +0300

    Добавьте файлы проекта.

---
+++ test-main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "catch2/catch_template_test_macros.hpp"
+#include "catch2/catch_session.hpp"
+#include "testHeader.h"
+
+
+TEST_CASE("test func", "[func List]") {
+    SECTION("testing func") {
+
+        List list;
+
+        CHECK(list.Empty() == 1);
+        CHECK(list.Size() == 0);
+
+        SECTION("Добавление 1 элемента") {
+            list.PushBack(10);
+
+            CHECK(list.Size() == 1);
+        }
+        SECTION("Удаление 1 элемента") {
+            list.Clear();
+
+            CHECK(list.Size() == 0);
+        }
+    }
+}
+
+int main()
+{
+	return Catch::Session().run();
+}
